18ae9d82448f5187ed2c8a2d7020b08d26feee14
M1rzayev-web <jaloliddintuitdif315@gmail.com>
2023-11-19 21:02:23 +0500

    ok

--- js/bazaFace.js
+++ js/bazaFace.js
@@ -1,5 +1,5 @@
 const baza = [
-    // Business: "Biznes",
+    // 
     // Premium: "Premium",
     // Sport: "Sport",
     // SportCar: "Sport Car",
@@ -45,8 +45,7 @@ const baza = [
         cars: {
             Sport: "Sport",
             SportCar: "Sport Car",
-            Business: "Biznes",
-            SUV: "Yo'l tanlamas",
+
         }
     },
     {
@@ -62,8 +61,7 @@ const baza = [
         cars: {
             Sport: "Sport",
             SportCar: "Sport Car",
-            Business: "Biznes",
-            SUV: "Yo'l tanlamas",
+
             Premium: "Premium"
 
 
@@ -82,7 +80,6 @@ const baza = [
         cars: {
             Sport: "Sport",
             SportCar: "Sport Car",
-            SUV: "Yo'l tanlamas",
             Comfort: "Komford"
 
         }
@@ -100,10 +97,9 @@ const baza = [
         cars: {
             Sport: "Sport",
             SportCar: "Sport Car",
-            SUV: "Yo'l tanlamas",
         }
     },
-     {
+    {
         id: 7,
         brand: "BMW",
         name: "BMW 8 SERIES 3.0 840I 2d",
@@ -152,7 +148,7 @@ const baza = [
             Comfort: "Komford",
             SUV: "Yo'l tanlamas",
             Premium: "Premium",
-            Business: "Biznes",
+
         }
     },
     {
@@ -169,7 +165,7 @@ const baza = [
             Comfort: "Komford",
             SUV: "Yo'l tanlamas",
             Premium: "Premium",
-            Business: "Biznes",
+
         }
     },
     {
@@ -186,7 +182,7 @@ const baza = [
             Comfort: "Komford",
             SUV: "Yo'l tanlamas",
             Premium: "Premium",
-            Business: "Biznes",
+
         }
     },
     {
@@ -203,88 +199,40 @@ const baza = [
             Comfort: "Komford",
             SUV: "Yo'l tanlamas",
             Premium: "Premium",
-            Business: "Biznes",
+
+        }
+    },
+    {
+        id: 13,
+        brand: "BMW",
+        name: "BMW Z4 2D",
+        price: 175,
+        img: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+        year: "2021",
+        region: {
+            Toshkent: "Toshkent",
+        },
+        cars: {
+            Sport: "Sport",
+            SportCar: "Sport Car",
         }
     },
 ]
 
-var dateInputF = document.getElementById('FirstData');
-var dateInputL = document.getElementById('LastData');
-
-dateInputF.addEventListener('change', function () {
-    var selectedDate = new Date(dateInputF.value);
-    var day = selectedDate.getDate();
-    var month = selectedDate.getMonth() + 1;
-    var year = selectedDate.getFullYear();
-    var formattedDate = year + '-' + ('0' + month).slice(-2) + '-' + ('0' + day).slice(-2);
-    dateInputF.value = formattedDate;
-    FirstDateFunc(formattedDate)
-});
-dateInputL.addEventListener('change', function () {
-    var selectedDate = new Date(dateInputL.value);
-    var day = selectedDate.getDate();
-    var month = selectedDate.getMonth() + 1;
-    var year = selectedDate.getFullYear();
-    var formattedDate = year + '-' + ('0' + month).slice(-2) + '-' + ('0' + day).slice(-2);
-    dateInputL.value = formattedDate;
-    LastDateFunc(formattedDate)
-});
-function FirstDateFunc(n) {
-    console.log("First data", n);
-}
-function LastDateFunc(n) {
-    console.log("Last data", n);
-}
-const check1 = document.getElementById("check1");
-const check2 = document.getElementById("check2");
-const check3 = document.getElementById("check3");
-const check4 = document.getElementById("check4");
-const check5 = document.getElementById("check5");
-const check6 = document.getElementById("check6");
-const btnSearch = document.querySelector(".btn__search");
-btnSearch.addEventListener("click", function () {
-    if (check1.checked === true) {
-        const selectedCars = baza.filter((item) => item.cars.SUV);
-        const selectedIds = selectedCars.map((item) => item);
-        console.log(selectedIds);
-    }
-    if (check2.checked === true) {
-        const selectedCars = baza.filter((item) => item.cars.Business);
-        const selectedIds = selectedCars.map((item) => item);
-        console.log(selectedIds);
-    }
-    if (check3.checked === true) {
-        const selectedCars = baza.filter((item) => item.cars.Premium);
-        const selectedIds = selectedCars.map((item) => item);
-        console.log(selectedIds);
-    }
-    if (check4.checked === true) {
-        const selectedCars = baza.filter((item) => item.cars.Sport);
-        const selectedIds = selectedCars.map((item) => item);
-        console.log(selectedIds);
-    }
-    if (check5.checked === true) {
-        const selectedCars = baza.filter((item) => item.cars.SportCar);
-        const selectedIds = selectedCars.map((item) => item);
-        console.log(selectedIds);
-    }
-    if (check6.checked === true) {
-        const selectedCars = baza.filter((item) => item.cars.Comfort);
-        const selectedIds = selectedCars.map((item) => item);
-        console.log(selectedIds);
-    }
-});
 const tabs = document.querySelectorAll(".box__btns button");
 const tabsParent = document.querySelector(".box__btns");
 const tabsContent = document.querySelectorAll(".tabcontent");
-
+const incBtn = document.querySelector(".inc__nav")
+const decBtn = document.querySelector(".dec__nav")
 function hideTabContent() {
     tabsContent.forEach((item) => {
         item.classList.add("DeActiveContent");
         item.classList.remove("ActiveContent");
     });
 
+
     tabs.forEach((item, i) => {
+
         item.classList.remove("activeBtns");
         const img = item.querySelector("img");
         if (img) {
@@ -294,6 +242,7 @@ function hideTabContent() {
 }
 
 function showTabContent(i = 0) {
+
     tabsContent.forEach((item) => {
         item.classList.add("DeActiveContent");
         item.classList.remove("ActiveContent");
@@ -325,163 +274,235 @@ tabs.forEach((tab, index) => {
 });
 
 const ListSUV = document.querySelector(".box__SUV-List")
-const ListBusness = document.querySelector(".box__Buss-List")
 const ListSport = document.querySelector(".box__Sport-List")
 const ListPremium = document.querySelector(".box__Prem-List")
 const ListComfort = document.querySelector(".box__Comf-List")
+const selectElement = document.querySelector("#numbers");
+
 function addSUV() {
     window.addEventListener("DOMContentLoaded", function () {
-        const CarSUV = baza.filter((item) => item.cars.SUV)
-        const Cars = CarSUV.map((item) => {
-            return `
-        <div class="box__Item">
-        <div class="box__item-Loc">
-            <p>${item.region.Toshkent}</p>
-        </div>
-        <div class="box__item-Img">
-            <img src="${item.img}" alt="${item.name}">
-        </div>
-        <h4 class="box__item-name">${item.name}</h4>
-        <div class="box__item-description">
-            <ul>
-                <li><p>${item.region.Toshkent}</p></li>
-                <li><img src="/img/logo/dvigitel.svg" alt=""><p>Toshkent</p></li>
-                <li><i class="fa-solid fa-gas-pump"></i><p>benzin</p></li>
-            </ul>
-        </div>
-        <div class="box__item-price">
-            <button>Bant qilish</button>
-            <p><span>${item.price}</span>$/kun</p>
-        </div>
-    </div>
-        `
-        }).join("")
-        ListSUV.innerHTML = Cars
-    })
+        const Cars = baza.filter((item) => item.cars.SUV);
+        let number = selectElement.value;
+
+        const loadMoreCars = function () {
+            const sliceNumber = Cars.slice(0, number);
+            const carsHTML = sliceNumber
+                .map((item) => {
+                    return `
+            <div class="box__Item">
+              <div class="box__item-Loc">
+                <p>Ташкент</p>
+              </div>
+              <div class="box__item-Img">
+                <img src="${item.img}" alt="${item.name}">
+              </div>
+              <h4 class="box__item-name">${item.name}</h4>
+              <div class="box__item-description">
+                <ul>
+                  <li><p>Ташкент</p></li>
+                  <li><img src="/img/logo/dvigitel.svg" alt=""><p>Toshkent</p></li>
+                  <li><i class="fa-solid fa-gas-pump"></i><p>benzin</p></li>
+                </ul>
+              </div>
+              <div class="box__item-price">
+                <button>Забронировать</button>
+                <p><span>${item.price}</span>$/kun</p>
+              </div>
+            </div>
+          `;
+                })
+                .join("");
+
+            ListSUV.innerHTML = carsHTML;
+
+        };
+
+        selectElement.addEventListener("change", function () {
+            number = selectElement.value;
+            loadMoreCars();
+        });
+
+        loadMoreCars();
+    });
 }
 
-function addBuss() {
-    window.addEventListener("DOMContentLoaded", function () {
-        const CarBuss = baza.filter((item) => item.cars.Business)
-        const Cars = CarBuss.map((item) => {
-            return `
-        <div class="box__Item">
-        <div class="box__item-Loc">
-            <p>${item.region.Toshkent}</p>
-        </div>
-        <div class="box__item-Img">
-            <img src="${item.img}" alt="${item.name}">
-        </div>
-        <h4 class="box__item-name">${item.name}</h4>
-        <div class="box__item-description">
-            <ul>
-                <li><p>${item.region.Toshkent}</p></li>
-                <li><img src="/img/logo/dvigitel.svg" alt=""><p>Toshkent</p></li>
-                <li><i class="fa-solid fa-gas-pump"></i><p>benzin</p></li>
-            </ul>
-        </div>
-        <div class="box__item-price">
-            <button>Bant qilish</button>
-            <p><span>${item.price}</span>$/kun</p>
-        </div>
-    </div>
-        `
-        }).join("")
-        ListBusness.innerHTML = Cars
-    })
-}
 function addSport() {
     window.addEventListener("DOMContentLoaded", function () {
-        const CarSport = baza.filter((item) => item.cars.SportCar)
-        const Cars = CarSport.map((item) => {
-            return `
-        <div class="box__Item">
-        <div class="box__item-Loc">
-            <p>${item.region.Toshkent}</p>
-        </div>
-        <div class="box__item-Img">
-            <img src="${item.img}" alt="${item.name}">
-        </div>
-        <h4 class="box__item-name">${item.name}</h4>
-        <div class="box__item-description">
-            <ul>
-                <li><p>${item.region.Toshkent}</p></li>
-                <li><img src="/img/logo/dvigitel.svg" alt=""><p>Toshkent</p></li>
-                <li><i class="fa-solid fa-gas-pump"></i><p>benzin</p></li>
-            </ul>
-        </div>
-        <div class="box__item-price">
-            <button>Bant qilish</button>
-            <p><span>${item.price}</span>$/kun</p>
-        </div>
-    </div>
-        `
-        }).join("")
-        ListSport.innerHTML = Cars
-    })
+        const Cars = baza.filter((item) => item.cars.Sport);
+        let number = selectElement.value;
+
+        const loadMoreCars = function () {
+            const sliceNumber = Cars.slice(0, number);
+            const carsHTML = sliceNumber
+                .map((item) => {
+                    return `
+              <div class="box__Item">
+                <div class="box__item-Loc">
+                  <p>Ташкент</p>
+                </div>
+                <div class="box__item-Img">
+                  <img src="${item.img}" alt="${item.name}">
+                </div>
+                <h4 class="box__item-name">${item.name}</h4>
+                <div class="box__item-description">
+                  <ul>
+                    <li><p>Ташкент</p></li>
+                    <li><img src="/img/logo/dvigitel.svg" alt=""><p>Toshkent</p></li>
+                    <li><i class="fa-solid fa-gas-pump"></i><p>benzin</p></li>
+                  </ul>
+                </div>
+                <div class="box__item-price">
+                  <button>Забронировать</button>
+                  <p><span>${item.price}</span>$/kun</p>
+                </div>
+              </div>
+            `;
+                })
+                .join("");
+
+            ListSport.innerHTML = carsHTML;
+        };
+
+        selectElement.addEventListener("change", function () {
+            number = selectElement.value;
+            loadMoreCars();
+        });
+
+        loadMoreCars();
+    });
 }
 function addPremium() {
     window.addEventListener("DOMContentLoaded", function () {
-        const CarPremium = baza.filter((item) => item.cars.Premium)
-        const Cars = CarPremium.map((item) => {
-            return `
-        <div class="box__Item">
-        <div class="box__item-Loc">
-            <p>${item.region.Toshkent}</p>
-        </div>
-        <div class="box__item-Img">
-            <img src="${item.img}" alt="${item.name}">
-        </div>
-        <h4 class="box__item-name">${item.name}</h4>
-        <div class="box__item-description">
-            <ul>
-                <li><p>${item.region.Toshkent}</p></li>
-                <li><img src="/img/logo/dvigitel.svg" alt=""><p>Toshkent</p></li>
-                <li><i class="fa-solid fa-gas-pump"></i><p>benzin</p></li>
-            </ul>
-        </div>
-        <div class="box__item-price">
-            <button>Bant qilish</button>
-            <p><span>${item.price}</span>$/kun</p>
-        </div>
-    </div>
-        `
-        }).join("")
-        ListPremium.innerHTML = Cars
-    })
+        const Cars = baza.filter((item) => item.cars.Premium);
+        let number = selectElement.value;
+
+        const loadMoreCars = function () {
+            const sliceNumber = Cars.slice(0, number);
+            const carsHTML = sliceNumber
+                .map((item) => {
+                    return `
+              <div class="box__Item">
+                <div class="box__item-Loc">
+                  <p>Ташкент</p>
+                </div>
+                <div class="box__item-Img">
+                  <img src="${item.img}" alt="${item.name}">
+                </div>
+                <h4 class="box__item-name">${item.name}</h4>
+                <div class="box__item-description">
+                  <ul>
+                    <li><p>Ташкент</p></li>
+                    <li><img src="/img/logo/dvigitel.svg" alt=""><p>Toshkent</p></li>
+                    <li><i class="fa-solid fa-gas-pump"></i><p>benzin</p></li>
+                  </ul>
+                </div>
+                <div class="box__item-price">
+                  <button>Забронировать</button>
+                  <p><span>${item.price}</span>$/kun</p>
+                </div>
+              </div>
+            `;
+                })
+                .join("");
+
+            ListPremium.innerHTML = carsHTML;
+        };
+
+        selectElement.addEventListener("change", function () {
+            number = selectElement.value;
+            loadMoreCars();
+        });
+
+        loadMoreCars();
+    });
 }
 function addComfort() {
     window.addEventListener("DOMContentLoaded", function () {
-        const CarComfort = baza.filter((item) => item.cars.Comfort)
-        const Cars = CarComfort.map((item) => {
-            return `
-        <div class="box__Item">
-        <div class="box__item-Loc">
-            <p>${item.region.Toshkent}</p>
-        </div>
-        <div class="box__item-Img">
-            <img src="${item.img}" alt="${item.name}">
-        </div>
-        <h4 class="box__item-name">${item.name}</h4>
-        <div class="box__item-description">
-            <ul>
-                <li><p>${item.region.Toshkent}</p></li>
-                <li><img src="/img/logo/dvigitel.svg" alt=""><p>Toshkent</p></li>
-                <li><i class="fa-solid fa-gas-pump"></i><p>benzin</p></li>
-            </ul>
-        </div>
-        <div class="box__item-price">
-            <button>Bant qilish</button>
-            <p><span>${item.price}</span>$/kun</p>
-        </div>
-    </div>
-        `
-        }).join("")
-        ListComfort.innerHTML = Cars
-    })
+        const Cars = baza.filter((item) => item.cars.Comfort);
+        let number = selectElement.value;
+
+        const loadMoreCars = function () {
+            const sliceNumber = Cars.slice(0, number);
+            const carsHTML = sliceNumber
+                .map((item) => {
+                    return `
+              <div class="box__Item">
+                <div class="box__item-Loc">
+                  <p>Ташкент</p>
+                </div>
+                <div class="box__item-Img">
+                  <img src="${item.img}" alt="${item.name}">
+                </div>
+                <h4 class="box__item-name">${item.name}</h4>
+                <div class="box__item-description">
+                  <ul>
+                    <li><p>Ташкент</p></li>
+                    <li><img src="/img/logo/dvigitel.svg" alt=""><p>Toshkent</p></li>
+                    <li><i class="fa-solid fa-gas-pump"></i><p>benzin</p></li>
+                  </ul>
+                </div>
+                <div class="box__item-price">
+                  <button>Забронировать</button>
+                  <p><span>${item.price}</span>$/kun</p>
+                </div>
+              </div>
+            `;
+                })
+                .join("");
+
+            ListComfort.innerHTML = carsHTML;
+        };
+
+        selectElement.addEventListener("change", function () {
+            number = selectElement.value;
+            loadMoreCars();
+        });
+
+        loadMoreCars();
+    });
 }
+
 addSUV();
-addBuss();
 addSport();
 addPremium();
 addComfort();
+
+const zayavfka = [
+    {
+        id: 1,
+        name: "Подать заявку",
+        deckription: "Оформляете заявку на сайте, связываетесь по телефону или в мессенджерах и бронируете автомобиль",
+        img: "/img/logo/zayavka.svg"
+    },
+    {
+        id: 2,
+        name: "Договор",
+        deckription: "Подписываете договор на аренду автомобиля, вносите оплату и страховой депозит",
+        img: "/img/logo/dogovor.svg"
+    },
+    {
+        id: 3,
+        name: "Получение",
+        deckription: "Забираете авто в нашем салоне или мы можем доставить в любую точку Москвы",
+        img: "/img/logo/poluchenie.svg"
+    }
+]
+function showKeyDeck() {
+    const showDiv = document.querySelector(".key__deck-List")
+
+    window.addEventListener("DOMContentLoaded", function () {
+        const data = zayavfka.map((item) => {
+            return `
+            <li>
+                    <span><img src="${item.img}" alt="logo"><p>${item.id}</p></span>
+                    <div>
+                        <h4>${item.name}</h4>
+                        <p>${item.deckription}</p>
+                    </div>
+                </li>
+            `
+        }).join("")
+        showDiv.innerHTML = data
+    })
+}
+showKeyDeck()
