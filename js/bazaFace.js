59e15b138aa2aa5b46649420cd9d2e780030c9fb
M1rzayev-web <jaloliddintuitdif315@gmail.com>
2023-12-05 22:09:50 +0500

    ok

--- js/bazaFace.js
+++ js/bazaFace.js
@@ -609,19 +609,77 @@ const hBurger = document.querySelector(".h__burger");
 const hBurgerX = document.querySelector(".h__burgerX");
 const hMobile = document.querySelector(".header__mobile");
 
-hBurger.addEventListener("click", function() {
+hBurger.addEventListener("click", function () {
     hMobile.style.display = "block";
-    setTimeout(function() {
+    setTimeout(function () {
         hMobile.style.transition = "transform 0.5s ease";
-  hMobile.style.transform = "translateX(0)";
-      }, 1);
-  
+        hMobile.style.transform = "translateX(0)";
+    }, 1);
+
 });
 
-hBurgerX.addEventListener("click", function() {
-  hMobile.style.transition = "transform 0.5s ease";
-  hMobile.style.transform = "translateX(-100%)";
-  setTimeout(function() {
-    hMobile.style.display = "none";
-  }, 1000);
+hBurgerX.addEventListener("click", function () {
+    hMobile.style.transition = "transform 0.5s ease";
+    hMobile.style.transform = "translateX(-100%)";
+    setTimeout(function () {
+        hMobile.style.display = "none";
+    }, 1000);
 });
+const KatalogList = document.querySelector(".Katalog__list");
+const selectKatalog = document.getElementById("K__select");
+selectKatalog.addEventListener("change", FilterAll);
+function FilterAll() {
+    let SelValue = selectKatalog.value;
+    let filteredData = [];
+
+    if (SelValue === "all") {
+        filteredData = baza.sort((a, b) => a.id - b.id);
+    } else if (SelValue === "min") {
+        filteredData = baza.sort((a, b) => a.price - b.price);
+    } else if (SelValue === "max") {
+        filteredData = baza.sort((a, b) => b.price - a.price);
+    } else if (SelValue === "new") {
+        filteredData = baza.sort((a, b) => new Date(b.date) - new Date(a.date));
+    } else if (SelValue === "AtoB") {
+        filteredData = baza.sort((a, b) => a.name.localeCompare(b.name));
+    }
+    const data = filteredData.map((item) => {
+        return `
+        <div class="box__Item" key="${item.id}">
+            <div class="box__item-Loc">
+                <p>Ташкент</p>
+            </div>
+            <div class="box__item-Img">
+                <img src="${item.img}" alt="${item.name}">
+            </div>
+            <h4 class="box__item-name">${item.name}</h4>
+            <div class="box__item-description">
+                <ul>
+                    <li><p>Ташкент</p></li>
+                    <li><img src="/img/logo/dvigitel.svg" alt=""><p>Toshkent</p></li>
+                    <li><i class="fa-solid fa-gas-pump"></i><p>benzin</p></li>
+                </ul>
+            </div>
+            <div class="box__item-price">
+                <button>Забронировать</button>
+                <p><span>${item.price}</span>$/kun</p>
+            </div>
+        </div>
+        `;
+    }).join("");
+
+    KatalogList.innerHTML = data;
+}
+const FirstMain = document.querySelector(".main")
+const FirstHero = document.querySelector(".hero")
+const boxBtnShow = document.querySelector(".box__btn")
+const KatalogMain = document.querySelector(".Katalog-main")
+boxBtnShow.addEventListener("click", function () {
+   showAuto()
+})
+function showAuto(){
+    KatalogMain.style.display = "block"
+    FirstMain.style.display = "none"
+    FirstHero.style.display = "none"
+    FilterAll()
+}
