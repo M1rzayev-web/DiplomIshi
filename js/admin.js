6daec8d081fa54e2145d1a09d6109261c0d7aeb0
M1rzayev-web <jaloliddintuitdif315@gmail.com>
2023-12-17 20:17:09 +0500

    server qo'shildi

--- js/admin.js
+++ js/admin.js
@@ -4,8 +4,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   submitBtn.addEventListener('click', function () {
     const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
     const price = document.getElementById('price').value;
     const carModel = document.getElementById('carModel').value;
 
