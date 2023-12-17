7fa951200a39c97a5788f7c35817d4f1c6afe5f5
M1rzayev-web <jaloliddintuitdif315@gmail.com>
2023-12-17 20:12:30 +0500

    server qo'shildi

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,33 @@
-// // admin.js
-// fetch('http://localhost:3000/api/cars')
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.error('Malumotlarni olishda xatolik:', error));
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('carRentalForm');
+  const submitBtn = document.getElementById('submitBtn');
+
+  submitBtn.addEventListener('click', function () {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const carModel = document.getElementById('carModel').value;
+    
+
+    fetch('http://localhost:3000/api/cars', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: generateUniqueId(), brand: "Toyota", name: carModel, price: 120, img: "/img/images/toyota-camry-white-2022.png", year: "2022", region: { Toshkent: "Toshkent", }, cars: { Sedan: "Sedan", Comfort: "Komfort" }, images: { 1: "/img/images/toyota-camry-white-2022.png", 2: "/img/images/toyota-camry-black-2022.png", 3: '/img/images/toyota-camry-blue-2022.png' } }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Serverdan qaytgan ma\'lumotlar:', data);
+        alert('Yuborildi! Rahmat!');
+      })
+      .catch(error => {
+        console.error('Serverga so\'rov jo\'natishda xatolik:', error);
+        alert('Xatolik yuz berdi! Qaytadan urinib ko\'ring.');
+      });
+  });
+});
+
+function generateUniqueId() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
--- js/server.js
+++ js/server.js
@@ -1,18 +1,32 @@
-  
 const express = require('express');
-const cors = require('cors');  // CORS modulini import qilib olish
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 const baza = require('./carData');
 
-app.use(cors());  // CORS ni o'rnatib olish
+app.use(cors());
+app.use(bodyParser.json());  // JSON formatdagi malumotlarni o'qish uchun
 
 app.get('/api/cars', (req, res) => {
   res.json(baza);
 });
 
+// POST so'rovni qabul qilish
+app.post('/api/cars', (req, res) => {
+  const newData = req.body;  // POST so'rov bilan kelgan malumotlar
+  // Malumotlarni qo'shish yoki qaytarish loyixasi
+  // ...
+
+  // Masalan, uni baza arrayiga qo'shib qo'ysa bo'ladi:
+  baza.push(newData);
+  
+  // Natija qaytarish
+  res.json({ success: true, message: 'Malumot qabul qilindi' });
+});
+
 app.listen(port, () => {
-  console.log(`Server is listening at https://localhost:${port}`);
+  console.log(`Server is listening at http://localhost:${port}`);
 });
