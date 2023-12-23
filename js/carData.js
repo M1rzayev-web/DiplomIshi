e0ce780b98e6eba7e580dc95077b5e45ad3ce11e
o3-noone <jaloliddintuitdif315@gmail.com>
2023-12-23 16:19:24 +0500

    ok

--- js/carData.js
+++ js/carData.js
@@ -1,282 +1,272 @@
 const baza = [
-    {
-        id: 1,
-        brand: "Chevrolet",
-        name: "Equinox",
-        price: 130,
-        img: "/img/images/1632489639.png",
-        year: "2022",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            SUV: "Yo'l tanlamas",
-            Comfort: "Komford"
-        },
-         images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+  {
+    id: 1,
+    brand: "Chevrolet",
+    name: "Equinox",
+    price: 130,
+    img: "/img/images/1632489639.png",
+    year: "2022",
+    region: {
+      Toshkent: "Toshkent",
     },
-    {
-        id: 2,
-        brand: "BMW",
-        name: "BMW 2 Series Gran Coupe",
-        price: 150,
-        img: "/img/images/bmw-2-gran-coupe-4d-white-2021.png",
-        year: "2021",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            SUV: "Yo'l tanlamas",
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+    cars: {
+      SUV: "Yo'l tanlamas",
+      Comfort: "Komford",
     },
-    {
-        id: 3,
-        brand: "BMW",
-        name: " BMW 4 Series 430i 2D Convertible Blue",
-        price: 165,
-        img: "/img/images/bmw-4-2d-coupe-blue-US-2021.png",
-        year: "2021",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            Sport: "Sport",
-            SportCar: "Sport Car",
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
+    },
+  },
+  {
+    id: 2,
+    brand: "BMW",
+    name: "BMW 2 Series Gran Coupe",
+    price: 150,
+    img: "/img/images/bmw-2-gran-coupe-4d-white-2021.png",
+    year: "2021",
+    region: {
+      Toshkent: "Toshkent",
+    },
+    cars: {
+      SUV: "Yo'l tanlamas",
+    },
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
+    },
+  },
+  {
+    id: 3,
+    brand: "BMW",
+    name: " BMW 4 Series 430i 2D Convertible Blue",
+    price: 165,
+    img: "/img/images/bmw-4-2d-coupe-blue-US-2021.png",
+    year: "2021",
+    region: {
+      Toshkent: "Toshkent",
+    },
+    cars: {
+      Sport: "Sport",
+      SportCar: "Sport Car",
+    },
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
+    },
+  },
+  {
+    id: 4,
+    brand: "BMW",
+    name: "BMW 5 Series 540i 4D",
+    price: 165,
+    img: "/img/images/bmw-5-4d-blue-US-2022.png",
+    year: "2022",
+    region: {
+      Toshkent: "Toshkent",
+    },
+    cars: {
+      Sport: "Sport",
+      SportCar: "Sport Car",
 
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+      Premium: "Premium",
     },
-    {
-        id: 4,
-        brand: "BMW",
-        name: "BMW 5 Series 540i 4D",
-        price: 165,
-        img: "/img/images/bmw-5-4d-blue-US-2022.png",
-        year: "2022",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            Sport: "Sport",
-            SportCar: "Sport Car",
-
-            Premium: "Premium"
-
-
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
     },
-    {
-        id: 5,
-        brand: "BMW",
-        name: "BMW 7 Series",
-        price: 165,
-        img: "/img/images/bmw-7-4d-silver-2021.png",
-        year: "2021",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            Sport: "Sport",
-            SportCar: "Sport Car",
-            Comfort: "Komford"
-
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+  },
+  {
+    id: 5,
+    brand: "BMW",
+    name: "BMW 7 Series",
+    price: 165,
+    img: "/img/images/bmw-7-4d-silver-2021.png",
+    year: "2021",
+    region: {
+      Toshkent: "Toshkent",
     },
-    {
-        id: 6,
-        brand: "BMW",
-        name: "BMW 3 Series 330i 4D",
-        price: 145,
-        img: "/img/images/bmw-330i-4d-blue-2021.png",
-        year: "2021",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            Sport: "Sport",
-            SportCar: "Sport Car",
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+    cars: {
+      Sport: "Sport",
+      SportCar: "Sport Car",
+      Comfort: "Komford",
     },
-    {
-        id: 7,
-        brand: "BMW",
-        name: "BMW 8 SERIES 3.0 840I 2d",
-        price: 175,
-        img: "/img/images/bmw-840i-2d-coupe-blue-2021.png",
-        year: "2021",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            Sport: "Sport",
-            SportCar: "Sport Car",
-            Premium: "Premium",
-            Comfort: "Komford"
-
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
     },
-    {
-        id: 8,
-        brand: "BMW",
-        name: "BMW 8 Series 840i 2D Convertible",
-        price: 175,
-        img: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
-        year: "2021",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            Sport: "Sport",
-            SportCar: "Sport Car",
-            Comfort: "Komford"
-
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+  },
+  {
+    id: 6,
+    brand: "BMW",
+    name: "BMW 3 Series 330i 4D",
+    price: 145,
+    img: "/img/images/bmw-330i-4d-blue-2021.png",
+    year: "2021",
+    region: {
+      Toshkent: "Toshkent",
     },
-    {
-        id: 9,
-        brand: "BMW",
-        name: "BMW X1",
-        price: 175,
-        img: "/img/images/bmw-x1-suv-white-2021.png",
-        year: "2021",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            Comfort: "Komford",
-            SUV: "Yo'l tanlamas",
-            Premium: "Premium",
-
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+    cars: {
+      Sport: "Sport",
+      SportCar: "Sport Car",
     },
-    {
-        id: 10,
-        brand: "BMW",
-        name: "BMW X3",
-        price: 175,
-        img: "/img/images/bmw-x3-suv-white-US-2021.png",
-        year: "2021",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            Comfort: "Komford",
-            SUV: "Yo'l tanlamas",
-            Premium: "Premium",
-
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
     },
-    {
-        id: 11,
-        brand: "BMW",
-        name: "BMW X5 40i 4D",
-        price: 175,
-        img: "/img/images/bmw-x5-40i-4d-suv-white-2021.png",
-        year: "2021",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            Comfort: "Komford",
-            SUV: "Yo'l tanlamas",
-            Premium: "Premium",
-
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+  },
+  {
+    id: 7,
+    brand: "BMW",
+    name: "BMW 8 SERIES 3.0 840I 2d",
+    price: 175,
+    img: "/img/images/bmw-840i-2d-coupe-blue-2021.png",
+    year: "2021",
+    region: {
+      Toshkent: "Toshkent",
     },
-    {
-        id: 12,
-        brand: "BMW",
-        name: "BMW XM 2024",
-        price: 175,
-        img: "/img/images/bmw-xm-suv-blue-US-2024.png",
-        year: "2024",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            Comfort: "Komford",
-            SUV: "Yo'l tanlamas",
-            Premium: "Premium",
-
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+    cars: {
+      Sport: "Sport",
+      SportCar: "Sport Car",
+      Premium: "Premium",
+      Comfort: "Komford",
     },
-    {
-        id: 13,
-        brand: "BMW",
-        name: "BMW Z4 2D",
-        price: 175,
-        img: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-        year: "2021",
-        region: {
-            Toshkent: "Toshkent",
-        },
-        cars: {
-            Sport: "Sport",
-            SportCar: "Sport Car",
-        },
-        images: {
-            1: "/img/images/bmw-xm-suv-blue-US-2024.png",
-            2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
-            3: '/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png'
-        }
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
     },
-]
+  },
+  {
+    id: 8,
+    brand: "BMW",
+    name: "BMW 8 Series 840i 2D Convertible",
+    price: 175,
+    img: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
+    year: "2021",
+    region: {
+      Toshkent: "Toshkent",
+    },
+    cars: {
+      Sport: "Sport",
+      SportCar: "Sport Car",
+      Comfort: "Komford",
+    },
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
+    },
+  },
+  {
+    id: 9,
+    brand: "BMW",
+    name: "BMW X1",
+    price: 175,
+    img: "/img/images/bmw-x1-suv-white-2021.png",
+    year: "2021",
+    region: {
+      Toshkent: "Toshkent",
+    },
+    cars: {
+      Comfort: "Komford",
+      SUV: "Yo'l tanlamas",
+      Premium: "Premium",
+    },
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
+    },
+  },
+  {
+    id: 10,
+    brand: "BMW",
+    name: "BMW X3",
+    price: 175,
+    img: "/img/images/bmw-x3-suv-white-US-2021.png",
+    year: "2021",
+    region: {
+      Toshkent: "Toshkent",
+    },
+    cars: {
+      Comfort: "Komford",
+      SUV: "Yo'l tanlamas",
+      Premium: "Premium",
+    },
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
+    },
+  },
+  {
+    id: 11,
+    brand: "BMW",
+    name: "BMW X5 40i 4D",
+    price: 175,
+    img: "/img/images/bmw-x5-40i-4d-suv-white-2021.png",
+    year: "2021",
+    region: {
+      Toshkent: "Toshkent",
+    },
+    cars: {
+      Comfort: "Komford",
+      SUV: "Yo'l tanlamas",
+      Premium: "Premium",
+    },
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
+    },
+  },
+  {
+    id: 12,
+    brand: "BMW",
+    name: "BMW XM 2024",
+    price: 175,
+    img: "/img/images/bmw-xm-suv-blue-US-2024.png",
+    year: "2024",
+    region: {
+      Toshkent: "Toshkent",
+    },
+    cars: {
+      Comfort: "Komford",
+      SUV: "Yo'l tanlamas",
+      Premium: "Premium",
+    },
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
+    },
+  },
+  {
+    id: 13,
+    brand: "BMW",
+    name: "BMW Z4 2D",
+    price: 175,
+    img: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+    year: "2021",
+    region: {
+      Toshkent: "Toshkent",
+    },
+    cars: {
+      Sport: "Sport",
+      SportCar: "Sport Car",
+    },
+    images: {
+      1: "/img/images/bmw-xm-suv-blue-US-2024.png",
+      2: "/img/images/bmw-Z4-2d-convertible-white-US-2021.png",
+      3: "/img/images/bmw-840i-convertible-2d-grey-open-US-2021.png",
+    },
+  },
+];
 module.exports = baza;
